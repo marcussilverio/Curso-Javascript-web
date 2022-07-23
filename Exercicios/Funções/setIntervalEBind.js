6df0886e1211e465ba6e51a414130eee259acd98
Marcus Silverio <marcus_vs@live.com>
2022-07-23 11:42:53 -0300

    Funcoes, diferencas de escopo com this e funcao bind

---
+++ Exercicios/Funções/setIntervalEBind.js
@@ -0,0 +1,27 @@
+function Pessoa(){
+    this.idade = 0
+    setInterval(function(){
+        this.idade++
+        console.log(this.idade)
+    },1000)
+}
+//new Pessoa
+
+function Pessoa2(){
+    this.idade = 0
+    setInterval(function(){
+        this.idade++
+        console.log(this.idade)
+    }.bind(this),1000)
+}
+//new Pessoa2
+
+function Pessoa3(){
+    this.idade = 0
+    const self = this
+    setInterval(function(){
+        self.idade++
+        console.log(self.idade)
+    },1000)
+}
+new Pessoa3
