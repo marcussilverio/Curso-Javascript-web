dd9b5dabf403a6e8868e2a5155698a76ab06df41
Marcus Silverio <marcus_vs@live.com>
2022-07-24 11:44:03 -0300

    tipos de declaracao de funcao

---
+++ Exercicios/Funções/tipoDeclaracao.js
@@ -0,0 +1,19 @@
+//function declaration
+function soma(x,y){
+    return x+y
+}
+
+//function expression
+const sub = function(x,y){
+    return x-y
+}
+//named function expression
+const mult = function mult(x,y){
+    return x*y
+}
+/*
+    function declaration, posso chamar em qualquer lugar do codigo independente
+    dela ja estar declarada ou nao.
+    tanto a expression quanto a named expression precisam estar declarada acima da
+    chamada para que funcione
+*/
