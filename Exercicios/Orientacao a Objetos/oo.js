a9946aa737ef1c04678a4273a417403c8f1a4cfb
Marcus Silverio - WLS2 UBUNTU 20.04 <marcus_vs@live.com>
2022-07-29 13:22:04 -0300

    Codigo nao executavel, conceitos e principios da orientacao a objetos

---
+++ Exercicios/Orientacao a Objetos/oo.js
@@ -0,0 +1,25 @@
+// CODIGO NAO EXECUTAVEL!!
+
+//Procedural
+function processamento(valor1, valor2, valor3){
+    //...
+}
+
+// OO
+objeto = {
+    valor1,
+    valor2, 
+    valor3,
+    processamento(){
+        //....
+    }
+}
+objeto.processamento()
+
+/*
+    PRINCIPIOS IMPORTANTES
+    * Abstracao
+    * Encapsulamento
+    * Herança (prototype)
+    * Polimorfismo
+*/
